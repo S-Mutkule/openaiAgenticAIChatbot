3f63916f2d242cb11d9f5ce62025ade60df6dd6f
Sarwadnya Mutkule <sarwadnyamutkule99@gmail.com>
2025-07-03 10:24:05 +0530

    first commit

---
+++ src/op_ag_ai/chatbot.py
@@ -0,0 +1,30 @@
+import chainlit as cl
+from agents import Agent, OpenAIChatCompletionsModel, Runner
+from openai import OpenAI, AsyncOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@cl.on_message
+async def main(message : cl.Message):
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
+    print(f"the message received is {message.content}")
+    print(f"the gemini api key is {gemini_api_key}")
+    client = AsyncOpenAI(
+        api_key=gemini_api_key, 
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+    model = OpenAIChatCompletionsModel(
+        model="gemini-2.0-flash",
+        openai_client=client
+    )
+    agent : Agent = Agent(
+        name="Assistant",
+        instructions="You are a helpful assistant that can answer questions and help with tasks.",
+        model=model
+    )
+    result = await Runner.run(agent, message.content)
+    await cl.Message(
+        content=result.final_output,
+    ).send()
